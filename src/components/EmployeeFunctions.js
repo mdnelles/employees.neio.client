b9ec4204507eb2fd43d816848f075e41e7f08adb
mdnelles <mxnelles@gmail.com>
2020-08-10 09:05:09 -0400

    update

--- src/components/EmployeeFunctions.js
+++ src/components/EmployeeFunctions.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
-import './config'; // adding config for folder specific build
+import axios from "axios";
+import "./config"; // adding config for folder specific build
 
 var thisServer = window.location.href;
 var serverPath = global.config.routerPath;
-if (thisServer.includes('3000')) serverPath = global.config.devPath;
+if (thisServer.includes("3000")) serverPath = global.config.devPath;
 
 export const addEmployee = (newEmployee, theToken) => {
    return axios
-      .post(serverPath + '/employee/register', {
+      .post(serverPath + "/employee/register", {
          uuid: newEmployee.uuid,
          first_name: newEmployee.first_name,
          last_name: newEmployee.last_name,
@@ -15,86 +15,86 @@ export const addEmployee = (newEmployee, theToken) => {
          password: newEmployee.password,
          admin: newEmployee.admin,
          token: theToken,
-         caller: 'EmployeeFunctions.register',
+         caller: "EmployeeFunctions.register",
       })
       .then((res) => {
-         console.log('Registered');
+         console.log("Registered");
          return res;
       })
       .catch((err) => {
          console.log(
-            'ClientSide Error @ EmployeeFunctions > getEmployees ' + err
+            "ClientSide Error @ EmployeeFunctions > getEmployees " + err
          );
-         return '++Error Loc 10';
+         return "++Error Loc 10";
       });
 };
 export const editEmployee = (editEmployee, theToken) => {
    return axios
-      .post(serverPath + '/employee/edit', {
+      .post(serverPath + "/employee/edit", {
          id: editEmployee.id,
          first_name: editEmployee.first_name,
          last_name: editEmployee.last_name,
          email: editEmployee.email,
          password: editEmployee.password,
          token: theToken,
-         caller: 'EmployeeFunctions.editEmployee',
+         caller: "EmployeeFunctions.editEmployee",
       })
       .then((res) => {
-         console.log('EmployeeFunctions.editEmployee');
+         console.log("EmployeeFunctions.editEmployee");
          return res;
       })
       .catch((err) => {
          console.log(
-            'ClientSide Error @ EmployeeFunctions.editEmployee ' + err
+            "ClientSide Error @ EmployeeFunctions.editEmployee " + err
          );
-         return '++Error Loc 10';
+         return "++Error Loc 10";
       });
 };
 
 export const removeEmployee = (id, token) => {
    return axios
-      .post(serverPath + '/employee/remove_employee', {
+      .post(serverPath + "/employee/remove_employee", {
          id,
          token,
-         caller: 'EmployeeFunctions.register',
+         caller: "EmployeeFunctions.register",
       })
       .then((res) => {
-         console.log('Employee Removed');
+         console.log("Employee Removed");
          return 1;
       })
       .catch((err) => {
          console.log(
-            'ClientSide Error @ EmployeeFunctions > removeEmployee ' + err
+            "ClientSide Error @ EmployeeFunctions.removeEmployee " + err
          );
-         return '++Error Loc 02';
+         return "EmployeeFunctions.removeEmployee";
       });
 };
 
 export const getEmployees = async (theToken) => {
    try {
-      const res = await axios.post(serverPath + '/employee/get_employees', {
+      const res = await axios.post(serverPath + "/employee/get_employees", {
          token: theToken,
-         caller: 'EmployeeFunctions.getEmployee',
+         caller: "EmployeeFunctions.getEmployee",
       });
       //console.log(res.data)
       return res.data;
    } catch (err) {
-      console.log('ClientSide Error @ EmployeeFunctions > getEmployees ' + err);
-      return '++Error Loc 07';
+      console.log("ClientSide Error @ EmployeeFunctions > getEmployees " + err);
+      return "++Error Loc 07";
    }
 };
 export const getDetails = async (id, theToken) => {
    try {
-      const res = await axios.post(serverPath + '/employee/get_details', {
+      const res = await axios.post(serverPath + "/employee/get_details", {
          id,
          token: theToken,
-         caller: 'EmployeeFunctions.details',
+         caller: "EmployeeFunctions.details",
       });
       console.log(res.data);
       return res.data;
    } catch (err) {
-      console.log('ClientSide Error @ EmployeeFunctions.getDetails ' + err);
-      return 'ClientSide Error @ EmployeeFunctions.getDetails ';
+      console.log("ClientSide Error @ EmployeeFunctions.getDetails " + err);
+      return "ClientSide Error @ EmployeeFunctions.getDetails ";
    }
 };
 //export const employeeIsLoggedIn = token => {
